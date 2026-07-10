4001c79801996d9ff91123201cc00284350aaa08
DaneRBK <DaneRBK@users.noreply.github.com>
2026-07-10 11:23:30 +0200

    Reset nudge from mobile center line command

--- SourceCode/GPS/Forms/MobileRemoteServer.cs
+++ SourceCode/GPS/Forms/MobileRemoteServer.cs
@@ -177,6 +177,11 @@ namespace AgOpenGPS
                     break;
 
                 case "nudge_center":
+                case "nudge_reset":
+                    if (trk.idx > -1) trk.NudgeDistanceReset();
+                    break;
+
+                case "snap_to_pivot":
                     if (trk.idx > -1) trk.SnapToPivot();
                     break;
 
